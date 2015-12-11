577cbb79482815bd2940dbec34e307f562f7c6c0
Bryan Fong <bryan.fch@gmail.com>
2015-12-11 18:28:01 +0800

    api Design - List, Create, Show done

--- app/controllers/api/api.js
+++ app/controllers/api/api.js
@@ -1,6 +1,8 @@
 var express  = require('express');
 var router   = express.Router();
 var mongoose = require('mongoose');
+var Design = require('../../models/design');
+
 
 module.exports = function (app) {
   app.use('/api', router);
@@ -18,4 +20,32 @@ function authenticatedUser(req, res, next) {
 
 router.get('/secret', authenticatedUser, function (req, res, next) {
   res.json({message: "secret"});
+});
+
+// design - index
+router.get('/designs', function(req, res, next){
+  Design.find({}, function(err, designs){
+    if (err) res.json({message : err})
+    res.json(designs);
+  })
+});
+
+// design - show
+router.get('/designs/:id', function(req, res, next){
+  var designId = req.params.id;
+  Design.findById(designId, function(err, design){
+    if (err) res.json({message : err})
+      res.json(design);
+  })
+});
+
+// design - create
+
+router.post('/designs', function(req, res, next){
+  var designParams = req.body.design;
+
+  Design.create(designParams, function(err, design){
+    if (err) res.json({message: err})
+      res.json({design : design})
+  })
 });
---
+++ app/models/design.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var DesignSchema = new Schema({
+  property_name: { type: String },
+  apartment_size: { type: String },
+  description: { type: String },
+  budget: { type: String } //,
+  // createdBy:   { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Design', DesignSchema);
